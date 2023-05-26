235a2c24c9d326db44aeaa75e8d7d6a36fac086b
omarnasser72 <105700689+omarnasser72@users.noreply.github.com>
2023-05-27 02:18:41 +0300

    Update stringFunctions2.c

--- stringFunctions2.c
+++ stringFunctions2.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * _strcpy - copy first and second strings
- * @dest: desination string
- * @src: source string
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
  *
- * Return: ptr to dest
-*/
+ * Return: pointer to destination
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -23,34 +23,34 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * _strdup - duplicates string
- * @str: string
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
  *
- * Return: ptr to str
-*/
+ * Return: pointer to the duplicated string
+ */
 char *_strdup(const char *str)
 {
 	int length = 0;
-	char *retn;
+	char *ret;
 
 	if (str == NULL)
 		return (NULL);
 	while (*str++)
 		length++;
-	retn = malloc(sizeof(char) * (length + 1));
-	if (!retn)
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
 		return (NULL);
 	for (length++; length--;)
-		retn[length] = *--str;
-	return (retn);
+		ret[length] = *--str;
+	return (ret);
 }
 
 /**
- * _puts - print string
- * @str: string
+ *_puts - prints an input string
+ *@str: the string to be printed
  *
- * Return: doesn't return
-*/
+ * Return: Nothing
+ */
 void _puts(char *str)
 {
 	int i = 0;
@@ -65,22 +65,23 @@ void _puts(char *str)
 }
 
 /**
- * _putchar - writes char to standard output
- * @c: char
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 1 if error occured return -1
-*/
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	static int i;
-	static char buffer[WRITE_BUF_SIZE];
+	static char buf[WRITE_BUF_SIZE];
 
 	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(1, buffer, i);
+		write(1, buf, i);
 		i = 0;
 	}
 	if (c != BUF_FLUSH)
-		buffer[i++] = c;
+		buf[i++] = c;
 	return (1);
 }
