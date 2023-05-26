55d63d733e47b201198ccf85757b998a7eb6bf15
Habiba-Moataz <nasrhabiba20@gmail.com>
2023-05-26 14:31:03 -0700

    env2

---
+++ env2.c
@@ -0,0 +1,89 @@
+#include "shell.h"
+
+/**
+ * get_environ - returns the string array copy of our environ
+ * @inf: Structure containing potential arguments. Used to maintain constant function prototype.
+ * Return: Always 0
+ */
+char **get_environ(info_t *inf)
+{
+	if (!inf->environ || inf->env_changed)
+	{
+		inf->environ = list_to_strings(inf->env);
+		inf->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @inf: Structure containing potential arguments. Used to maintain constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @v: the string env var property
+ */
+int _unsetenv(info_t *inf, char *v)
+{
+	list_t *node = inf->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !v)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, v);
+		if (p && *p == '=')
+		{
+			inf->env_changed = delete_node_at_index(&(inf->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (inf->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,or modify an existing one
+ * @inf: Structure containing potential arguments. Used to maintain constant function prototype.
+ * @v: the string env var property
+ * @val: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(info_t *inf, char *v, char *val)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!v || !val)
+		return (0);
+
+	buf = malloc(_strlen(v) + _strlen(val) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, v);
+	_strcat(buf, "=");
+	_strcat(buf, val);
+	node = inf->env;
+	while (node)
+	{
+		p = starts_with(node->str, v);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(inf->env), buf, 0);
+	free(buf);
+	inf->env_changed = 1;
+	return (0);
+}
